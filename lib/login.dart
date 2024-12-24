9b4e795d08f0e6c25d9c998e933174ed1ff1008d
AbihaRiaz1703 <ramshariaz039@gmail.com>
2024-12-24 15:34:08 +0500

    initial comment

--- lib/login.dart
+++ lib/login.dart
@@ -16,164 +16,159 @@ class _LoginState extends State<Login> {
     return DefaultTabController(
         length: 4,
         child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          backgroundColor: const Color.fromARGB(255, 212, 209, 216),
-          body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 67,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image(
-                      height: 65,
-                      width: 90,
-                      image: AssetImage('images/logo.png'),
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Column(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: const Color.fromARGB(255, 228, 199, 155),
+            body: SafeArea(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  const SizedBox(
+                    height: 67,
+                  ),
+                  const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'Coding\'z',
-                          style: TextStyle(
-                            color: Colors.orange,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 26,
-                          ),
+                        CircleAvatar(
+                          backgroundImage: AssetImage('images/log.png'),
                         ),
-                        Text(
-                          'Keyz',
-                          style: TextStyle(
-                            color: Colors.orange,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 26,
-                          ),
+                      ]),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Dinner\'s',
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26,
                         ),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 56,
-                ),
-                const Center(
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.orange,
-                      fontSize: 26,
-                    ),
+                      ),
+                      Text(
+                        'Ready',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.orange,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Center(
-                  child: Text(
-                    textAlign: TextAlign.center,
-                    'Food is Love & Love iS Food.',
-                    style: TextStyle(
-                      color: Colors.orange,
-                      fontSize: 10,
-                    ),
+                  const SizedBox(
+                    height: 56,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10, right: 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      hintStyle: const TextStyle(
-                        color: Color.fromARGB(255, 212, 209, 216),
-                      ),
-                      fillColor: Colors.orange,
-                      filled: true,
-                      prefix: const Icon(
-                        Icons.email_rounded,
-                        color: Color.fromARGB(255, 177, 160, 214),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.orange),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.orange),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10, right: 10),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: const TextStyle(
-                        color: Color.fromARGB(255, 212, 209, 216),
-                      ),
-                      fillColor: Colors.orange,
-                      filled: true,
-                      prefix: const Icon(
-                        Icons.ac_unit_rounded,
-                        color: Color.fromARGB(255, 177, 160, 214),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.orange),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const BorderSide(color: Colors.orange),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, Practice.id);
-                    },
-                    child: Text('Let\'s Go!')),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Haven\'t got account yet?',
-                      textAlign: TextAlign.center,
+                  const Center(
+                    child: Text(
+                      'Login',
                       style: TextStyle(
-                        color: Colors.black,
+                        color: Colors.orange,
+                        fontSize: 26,
                       ),
                     ),
-                    Text(
-                      'sign up?',
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  const Center(
+                    child: Text(
                       textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.orange),
+                      'Food is Love & Love iS Food.',
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontSize: 10,
+                      ),
                     ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ));
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        hintStyle: const TextStyle(
+                          color: Color.fromARGB(255, 212, 209, 216),
+                        ),
+                        fillColor: Colors.orange,
+                        filled: true,
+                        prefix: const Icon(
+                          Icons.email_rounded,
+                          color: Color.fromARGB(255, 177, 160, 214),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.orange),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.orange),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10, right: 10),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: const TextStyle(
+                          color: Color.fromARGB(255, 212, 209, 216),
+                        ),
+                        fillColor: Colors.orange,
+                        filled: true,
+                        prefix: const Icon(
+                          Icons.ac_unit_rounded,
+                          color: Colors.orange,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.orange),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.orange),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, Practice.id);
+                      },
+                      child: Text('Let\'s Go!')),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Haven\'t got account yet?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.orange,
+                        ),
+                      ),
+                      Text(
+                        'sign up?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.orange),
+                      ),
+                    ],
+                  ),
+                ]))));
   }
 }
